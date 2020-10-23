6ca71afcaf290f8f105cc932269f12908265858d
ariqbasyar <ariqbasyar2@gmail.com>
2020-10-23 19:52:12 +0700

    shutdown after sent all data

--- nodes/master/app.py
+++ nodes/master/app.py
@@ -5,5 +5,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(SOCK)
     print('Connected to', SOCK)
     s.sendall('hello my slave'.encode())
+    s.shutdown(socket.SHUT_WR)
     reply = s.recv(1024)
     print(reply.decode())
