b36a215239b9d634722d0e881a7e06a4f4af9717
ariqbasyar <ariqbasyar2@gmail.com>
2020-10-24 20:11:19 +0700

    move to folder component

--- nodes/worker/app.py
+++ nodes/worker/app.py
@@ -3,6 +3,6 @@ import utils
 from time import sleep
 from threading import Thread
 from enum import Enum
-from . import availibility, cancel, executor, status
+from component import availibility, cancel, executor, status
 
 # TODO bikin thread masing-masing port
